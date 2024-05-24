9f71290aca70dbe42bd0010b10b7a97911eb3304
Frothy <76622149+FrothyMarrow@users.noreply.github.com>
2024-05-24 04:43:18 -0400

    feat: render add triangles draw call

--- src/render.zig
+++ src/render.zig
@@ -13,6 +13,10 @@ pub const Color = struct {
 };
 
 pub const Renderer = struct {
+    vertexArray: u32,
+    vertexBuffer: u32,
+    length: u32,
+
     pub fn clearColor(_: Renderer, color: Color) void {
         c.glClear(c.GL_COLOR_BUFFER_BIT);
         c.glClearColor(color.r, color.g, color.b, color.a);
@@ -21,8 +25,36 @@ pub const Renderer = struct {
     pub fn useShader(_: Renderer, shader_program: shader.Shader) void {
         c.glUseProgram(shader_program.program_id);
     }
+
+    pub fn drawTriangles(self: *Renderer, vertices: []const f32) void {
+        self.length = @intCast(vertices.len);
+
+        c.glGenBuffers(1, &self.vertexBuffer);
+
+        c.glGenVertexArrays(1, &self.vertexArray);
+        c.glBindVertexArray(self.vertexArray);
+
+        c.glBindBuffer(c.GL_ARRAY_BUFFER, self.vertexBuffer);
+        c.glBufferData(c.GL_ARRAY_BUFFER, @intCast(vertices.len * @sizeOf(f32)), vertices.ptr, c.GL_STATIC_DRAW);
+
+        c.glVertexAttribPointer(0, 3, c.GL_FLOAT, c.GL_FALSE, 3 * @sizeOf(f32), @ptrFromInt(0));
+        c.glEnableVertexAttribArray(0);
+    }
+
+    pub fn render(self: Renderer) void {
+        c.glDrawArrays(c.GL_TRIANGLES, 0, @intCast(self.length / 3));
+    }
+
+    pub fn deinit(self: Renderer) void {
+        c.glDeleteVertexArrays(1, &self.vertexArray);
+        c.glDeleteBuffers(1, &self.vertexBuffer);
+    }
 };
 
 pub fn create() Renderer {
-    return Renderer{};
+    return Renderer{
+        .vertexArray = 0,
+        .vertexBuffer = 0,
+        .length = 0,
+    };
 }
